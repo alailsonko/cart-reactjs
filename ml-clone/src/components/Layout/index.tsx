9ec2a0e6d326f181b94cf06a775e772293fd317f
alailsonko <aalailson3@gmail.com>
2020-09-16 23:49:51 -0300

    conclude the clone

--- ml-clone/src/components/Layout/index.tsx
+++ ml-clone/src/components/Layout/index.tsx
@@ -2,16 +2,18 @@ import React from "react";
 
 import { Container, Wrapper } from "./styles";
 import Product from "../Product";
+import Header from "../Header";
+import Footer from "../Footer";
 
 const Layout: React.FC = () => {
   return (
     <Container>
-      {/* <Header /> */}
+      <Header />
       <Wrapper>
         <Product />
       </Wrapper>
 
-      {/* <Footer /> */}
+      <Footer />
     </Container>
   );
 };
