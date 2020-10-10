ab0e693470d8546a4ab9d3ec2f981f95d5861341
alailsonko <aalailson3@gmail.com>
2020-10-10 16:05:36 -0300

    typeform: conclude

--- typeform-clone/src/App.tsx
+++ typeform-clone/src/App.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
 
+import GlobalStyles from './styles/global'
+import { Main, Section, Sticky } from './styles'
+
+import FirstAndSecond  from './components/FirstAndSecond'
+
 function App() {
+  React.useEffect(() => {
+    window.scrollTo({ top: 0})
+  },[])
   return (
-    <div>
-       <h2>hello world</h2>
-    </div>
+    <> 
+     <GlobalStyles />   
+     <Main style={{height: '1610vh'}} >
+       <Section style={{ height: '72.7%' }} >
+           <FirstAndSecond />
+       </Section>
+       <Section style={{ height: '9.7%'}}>
+         <Sticky className="third" />
+       </Section>
+       <Section style={{ height: '10.1%'}}>
+         <Sticky className="fourth" />
+       </Section>
+     </Main>
+  
+    </>
   );
 }
 
