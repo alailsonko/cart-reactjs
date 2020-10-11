920f04c3bf3bc8b255a882d2f15ccb57b9cc9a55
alailsonko <aalailson3@gmail.com>
2020-10-10 22:03:24 -0300

    linkedin-close: updating

--- linkedin-clone/src/App.tsx
+++ linkedin-clone/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import GlobalStyles from './styles/GlobalStyles';
+import Layout from './components/Layout'
 
 function App() {
   return (
     <>
-      <h1>Rocketseat</h1>
+      <Layout />
 
       <GlobalStyles />
     </>
--- linkedin-clone/src/components/LeftColumn/HashtagPanel/index.tsx
+++ linkedin-clone/src/components/LeftColumn/HashtagPanel/index.tsx
@@ -4,7 +4,7 @@ import Panel from '../../Panel';
 
 import { Container, HashtagIcon } from './styles';
 
-const tags = ['rocketseat', 'next-level', 'node', 'react', 'developer'];
+const tags = ['none', 'none', 'node', 'react', 'developer'];
 
 const HashtagPanel: React.FC = () => {
   return (
