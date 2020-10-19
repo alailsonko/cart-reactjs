9d3ca0ea4dafde4fbd51db8cdf3d65418fe3d091
alailsonko <aalailson3@gmail.com>
2020-10-19 16:18:15 -0300

    test: implements global style

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,8 +1,19 @@
-// eslint-disable-next-line no-unused-vars
-import render from 'react-dom';
+import { shallow } from 'enzyme';
+import React from 'react';
+import Routes from 'routes';
+import GlobalStyles from 'styles/GlobalStyles';
+import App from './App';
 
-describe('Name of the group', () => {
-  test('should', () => {
-    expect(3).toBe(3);
+const wrapper = shallow(<App />);
+
+describe('App component', () => {
+  test('should render GlobalStyles and Routes', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+  test('should match component GlobalStyles', () => {
+    expect(wrapper.find(<GlobalStyles />)).toMatchSnapshot();
+  });
+  test('should match component Routes', () => {
+    expect(wrapper.find(<Routes />)).toMatchSnapshot();
   });
 });
