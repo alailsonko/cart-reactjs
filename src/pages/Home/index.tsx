8f979ba6bf1c92eb77ecd139b5d04d181051eb6a
alailsonko <aalailson3@gmail.com>
2020-10-20 20:50:07 -0300

    feat: fetching data is working

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,73 @@
-import React from 'react';
+/* eslint-disable import/no-dynamic-require */
+/* eslint-disable global-require */
+import React, { useEffect, useState } from 'react';
+import axios, { AxiosResponse } from 'axios';
 
 import { Container } from './styles';
 
-const Home: React.FC = () => (
-  <Container>
-    <h1>Home</h1>
-  </Container>
-);
+interface ProductsDTO {
+   id: string;
+   sku: string;
+   title: string;
+   description: string;
+   availableSizes: string[];
+   price: number;
+   isFreeShipping: boolean;
+   quantity: number;
+   category: string;
+   image: string;
+}
+
+function Home() {
+  const [products, setProducts] = useState<Promise<ProductsDTO[]>>();
+  const [loading, setLoading] = useState(true);
+
+  // useEffect(() => {
+  //   const response = axios.get('http://localhost:3004/products/')
+  //     .then(async (res: AxiosResponse): Promise<ProductsDTO[]> => Promise.resolve(res.data))
+  //     .catch((err) => { throw new Error(err.message); });
+
+  //   setProducts(response);
+  //   console.log(products);
+  // },
+  // []);
+
+  useEffect(() => {
+    async function loadData(): Promise<void> {
+      const response: AxiosResponse = await axios.get('http://localhost:3004/products/');
+
+      if (response.data) {
+        setProducts(response.data);
+      }
+      setLoading(false);
+    }
+
+    loadData();
+  }, []);
+
+  // eslint-disable-next-line no-console
+  // console.log(process.env.PUBLIC_URL);
+
+  return loading ? (
+    <Container>
+      <h2>loading...</h2>
+    </Container>
+  ) : (
+    <Container>
+      Home
+      {
+       products instanceof Array ? (products.map((item: ProductsDTO) => (
+         <div key={item.id}>
+
+           <h2>{item.id}</h2>
+
+           <img src={`${item.image}`} style={{ width: '100px', height: '100px' }} alt="" />
+         </div>
+       ))
+       ) : (null)
+      }
+    </Container>
+  );
+}
 
 export default Home;
