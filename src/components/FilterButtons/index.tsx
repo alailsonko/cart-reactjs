9404359ae77a428ca290762540ef4c45da601d8e
alailsonko <aalailson3@gmail.com>
2020-12-28 01:47:46 -0300

    feat: filterBbutons component start

---
+++ src/components/FilterButtons/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const FilterButtons: React.FC = () => {
+  console.log('hello world');
+  return (
+    <Container>
+      <h2>hello world</h2>
+
+    </Container>
+  );
+};
+
+export default FilterButtons;
